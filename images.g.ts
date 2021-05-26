07867a9f490da2b3c15b1c23051b9e47cb1a19dc
kokoro000 <84844332+kokoro000@users.noreply.github.com>
2021-05-26 18:02:42 +0800

    MakeCode 專案的初始檔案

---
+++ images.g.ts
@@ -0,0 +1,113 @@
+// 自動生成的代碼。請勿編輯。
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "2Kvz$#uK8Oy|9UX8**5,":
+            case "槌子":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . d d d . . . . . 
+. . . . . . . 4 4 4 d . . . . . 
+. . . . . . 4 4 4 4 d . . . . . 
+. . . . . 4 4 4 4 4 d . . . . . 
+. . . . 4 4 4 4 4 4 . . . . . . 
+. . . 4 4 4 4 4 4 . . . . . . . 
+. . d d d d 4 4 4 4 . . . . . . 
+. d 4 4 4 d 4 4 4 4 d . . . . . 
+. d 4 4 4 d . 4 4 d 4 4 . . . . 
+. d d d d . . . d 4 4 4 . . . . 
+. . . . . . . . . 4 4 4 4 4 4 . 
+. . . . . . . . . . 4 4 d d 4 . 
+. . . . . . . . . . . 4 d d 4 . 
+. . . . . . . . . . . 4 4 4 4 . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "![;Mr4?|0pcFZ5~3!PwB":
+            case "基本款":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . . f e e e e e e f . . . . 
+. . . f e 1 e e 1 e e e f . . . 
+. . f e e f e e f e e e e f . . 
+. . f e e f e e f e e e e f . . 
+. f e e e e e e e e e e e e f . 
+. f e e 1 2 2 e e e e e e e f . 
+. f e 1 1 2 2 e e e e e e e f . 
+. f e 2 2 2 2 e e e e e e e f . 
+. f e e 2 2 e e e e e e e e f . 
+. f e e e e e e e e e e e e f . 
+. f e e e e e e e e e e e e f . 
+. f e e e e e e e e e e e e f . 
+. f e e e e e e e e e e e e f . 
+`;
+            case "WKcD4(?I:=;pd7hUG4|?":
+            case "芋頭":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f b b b b f f . . . . 
+. . . . f b b b b b b f . . . . 
+. . . f b 1 b b 1 b b b f . . . 
+. . f b b a b b a b b b b f . . 
+. . f b b a b b a b b b b f . . 
+. f b b b b b b b b b b b b f . 
+. f b b 1 3 3 b b b b b b b f . 
+. f b 1 1 3 3 b b b b b b b f . 
+. f b 3 3 3 3 b b b b b b b f . 
+. f b b 3 3 b b b b b b b b f . 
+. f b b b b b b b b b b b b f . 
+. f b b b b b b b b b b b b f . 
+. f b b b b b b b b b b b b f . 
+. f b b b b b b b b b b b b f . 
+`;
+            case "Y^Yti%tS":
+            case "殭屍":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f 7 7 7 7 f f . . . . 
+. . . . f 7 7 7 7 7 7 f . . . . 
+. . . f 7 1 7 7 1 7 7 7 f . . . 
+. . f 7 7 2 7 7 2 7 7 7 7 f . . 
+. . f 7 7 2 7 7 2 7 7 7 7 f . . 
+. f 7 7 7 7 7 7 7 7 7 7 7 7 f . 
+. f 7 7 5 2 2 7 7 7 7 7 7 7 f . 
+. f 7 5 5 2 2 7 7 7 7 7 7 7 f . 
+. f 7 2 2 2 2 7 7 7 7 7 7 7 f . 
+. f 7 7 2 2 7 7 7 7 7 7 7 7 f . 
+. f 7 7 7 7 7 7 7 7 7 7 7 7 f . 
+. f 7 7 7 7 7 7 7 7 7 7 7 7 f . 
+. f 7 7 7 7 7 7 7 7 7 7 7 7 f . 
+. f 7 7 7 7 7 7 7 7 7 7 7 7 f . 
+`;
+            case "aFD,5L+iuAvVXEf4_VJ)":
+            case "小辣椒":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f 2 2 2 2 f . . . . . 
+. . . . f 2 2 2 2 2 2 f . . . . 
+. . . f 2 1 2 2 1 2 2 2 f . . . 
+. . f 2 2 f 2 2 f 2 2 2 2 f . . 
+. . f 2 2 f 2 2 f 2 2 2 2 f . . 
+. f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 1 4 4 2 2 2 2 2 2 2 f . 
+. f 2 1 1 4 4 2 2 2 2 2 2 2 f . 
+. f 2 4 4 4 4 2 2 2 2 2 2 2 f . 
+. f 2 2 4 4 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+. f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// 自動生成的代碼。請勿編輯。
